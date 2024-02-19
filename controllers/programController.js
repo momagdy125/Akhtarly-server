d9f233960cd7b6470ab3bd2bc6ba85943ac55f88
HP <mohamedmaggdy125@gmail.com>
2024-02-19 17:35:19 +0200

    get lowest rank cpu and gpu

--- controllers/programController.js
+++ controllers/programController.js
@@ -16,7 +16,6 @@ exports.getAllPrograms = (request, response, next) => {
   Query = RemovingFieldsFromQuery(Query, ["limit", "sort", "page"]);
 
   const DefaultLimit = pagination(request);
-
   programModel
     .find(Query, { __v: false })
     .sort(request.query.sort)
@@ -71,11 +70,41 @@ exports.deleteProgram = (request, response, next) => {
     });
 };
 
-exports.sendPrograms = (request, response, next) => {
-  programModel
-    .find({ _id: { $in: request.body.programList } })
-    .select("MinGPU MinCPU")
-    .then((programs) => {
-      response.send(programs);
+exports.sendPrograms = async (request, response, next) => {
+  try {
+    const programList = request.body.programList;
+
+    // Search for programs and extract CPU Model names
+    const programs = await programModel
+      .find({ _id: { $in: programList } })
+      .select("MinCPU MinGPU");
+
+    const minCPUArray = programs.map((program) => program.MinCPU);
+    const minGPUArray = programs.map((program) => program.MinGPU);
+
+    // Search for CPU document using the Model field based on minCPUArray
+    const cpu = (
+      await cpuModel
+        .find({
+          Model: { $in: minCPUArray },
+        })
+        .limit(1)
+    )[0];
+    const gpu = (
+      await gpuModel
+        .find({
+          Model: { $in: minGPUArray },
+        })
+        .limit(1)
+    )[0];
+
+    response.json({
+      result: {
+        cpu,
+        gpu,
+      },
     });
+  } catch (error) {
+    next(new apiError(error.message, 400));
+  }
 };
