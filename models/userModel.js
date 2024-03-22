1abd03bf5e12a0f07c506f4ac0f83b25c0dd7a0f
HP <mohamedmaggdy125@gmail.com>
2024-03-22 17:14:33 +0200

    fix login bug

--- models/userModel.js
+++ models/userModel.js
@@ -46,7 +46,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
-    this.password = bcrypt.hash(this.password, 12);
+    this.password = await bcrypt.hash(this.password, 12);
     this.changePassAt = Math.floor(Date.now() / 1000);
   }
   if (this.isNew) {
